913130ee4ccf118640a8ac4d2abdfcd2a9a02664
André Lashley <andre.lashley@gmail.com>
2024-12-18 13:29:06 -0800

    Add particle system

---
+++ ParticleSystem/ConfettiStar.cs
@@ -0,0 +1,23 @@
+﻿using SkiaSharp.Extended.UI.Controls;
+using SkiaSharp.Extended;
+using SkiaSharp;
+
+namespace Particle.Maui.ParticleSystem
+{
+    public class ConfettiStar : SKConfettiShape
+    {
+        public ConfettiStar(int points)
+        {
+            Points = points;
+        }
+
+        public int Points { get; }
+
+        protected override void OnDraw(SKCanvas canvas, SKPaint paint, float size)
+        {
+            using var star = SKGeometry.CreateRegularStarPath(size, size / 2, Points);
+
+            canvas.DrawPath(star, paint);
+        }
+    }
+}
